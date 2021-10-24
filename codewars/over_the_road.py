cc08630f9fff35b48966f46df510886f7ce56c5d
Mudhya19 <88097504+Mudhya19@users.noreply.github.com>
2021-10-24 21:16:58 +0800

    Delete codewars directory

--- codewars/over_the_road.py
+++
@@ -1,5 +0,0 @@
-def number_of_hooks(length,max_hook_dist):
-    n = 1
-    while length/n > max_hook_dist:
-        n = n*2
-    return n + 1
--- codewars/triangle_area.py
+++
@@ -1,7 +0,0 @@
-t_area = {'\n.\n. .\n. . .\n. . . .\n. . . . .\n. . . . . .\n. . . . . . .\n. . . . . . . .\n. . . . . . . . .\n'}
-
-
-def t_area(a):
-    cut = a.split("\n")
-    cal = cut[-2].count(' ')
-    return ((cal*cal)/2)
