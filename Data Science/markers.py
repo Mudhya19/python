d83a23d7b5b6fa03888b0cfbb8fe26e1e9776e2d
Mudhya19 <88097504+Mudhya19@users.noreply.github.com>
2021-10-24 21:23:49 +0800

    Delete Data Science directory

--- Data Science/markers.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import colorbar
-import matplotlib.pyplot as plt
-import numpy as np
-
-ypoints = np.array([3, 8, 1, 10, 3, 7, 3, 9, 7, 11])
-
-plt.plot(ypoints, marker='o')
-plt.show()
